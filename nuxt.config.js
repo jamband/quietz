b035df0a29edfef65413e68a9eae90833740ad36
Tomoki Morita <tmsongbooks215@gmail.com>
2021-05-29 16:26:29 +0900

    Remove pwa package
    and some fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,10 @@
-import { APP_NAME, APP_DESCRIPTION, APP_URL } from './plugins/constants'
-
 export default {
   ssr: true,
   target: 'static',
   head: {
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-
-      { hid: 'og:site_name', property: 'og:site_name', content: APP_NAME },
-      { hid: 'og:type', property: 'og:type', content: 'website' },
-      { hid: 'og:url', property: 'og:url', content: APP_URL }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -26,7 +20,6 @@ export default {
     { path: '~/icons', pathPrefix: false }
   ],
   buildModules: [
-    '@nuxtjs/pwa',
     'nuxt-purgecss'
   ],
   messages: {
@@ -40,15 +33,6 @@ export default {
     routes: ['/']
   },
   // top level options for packages
-  manifest: {
-    name: APP_NAME,
-    short_name: APP_NAME,
-    description: APP_DESCRIPTION,
-    lang: 'en-US',
-    background_color: '#fff',
-    display: 'standalone',
-    start_url: '/'
-  },
   purgeCSS: {
     // enabled: true,
     paths: [
