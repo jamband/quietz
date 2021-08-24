58e8d88727c82cf8647f9f21581416c7e53915d1
Tomoki Morita <tmsongbooks215@gmail.com>
2021-08-25 02:57:21 +0900

    Improve some layout styles

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,15 +1,15 @@
 <template>
   <footer class="fixed-bottom py-3 text-center bg-light">
-    <nav class="container" aria-label="Footer navigation">
+    <nav class="container" :class="$style.contents" aria-label="Footer navigation">
       <ul class="list-inline mb-0">
         <li class="list-inline-item">
-          <NLink :to="{ name: 'about' }">About</NLink>
+          <NLink :to="{ name: 'about' }" class="px-2 py-2">About</NLink>
         </li>
         <li class="list-inline-item">
-          <NLink :to="{ name: 'contact' }">Contact</NLink>
+          <NLink :to="{ name: 'contact' }" class="px-2 py-2">Contact</NLink>
         </li>
         <li class="list-inline-item">
-          <a :href="app.repositoryUrl" rel="noopener" target="_blank">GitHub</a>
+          <a :href="app.repositoryUrl" class="px-2 py-2" rel="noopener" target="_blank">GitHub</a>
         </li>
       </ul>
     </nav>
@@ -29,3 +29,9 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" module>
+.contents {
+  padding-bottom: env(safe-area-inset-bottom);
+}
+</style>
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="fixed-top py-3 text-center bg-light">
     <nav class="container" aria-label="Header navigation">
-      <NLink :to="{ name: 'index' }" class="text-decoration-none">{{ app.name }}</NLink>
+      <NLink :to="{ name: 'index' }" class="px-3 py-2 text-decoration-none">{{ app.name }}</NLink>
     </nav>
   </header>
 </template>
