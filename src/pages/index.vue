5228cd5952af675764ad02cbed0ac715051a5178
Tomoki Morita <tmsongbooks215@gmail.com>
2021-11-04 23:19:22 +0900

    Use const instead of useFetch

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
-const { data: routes } = await useFetch("/api/home/routes");
+// const { data: routes } = await useFetch("/api/home/routes");
+const routes = [
+  { name: "alert", text: "Alert" },
+  { name: "collapse", text: "Collapse" },
+  { name: "dropdown", text: "Dropdown" },
+  { name: "form", text: "Form" },
+  { name: "modal", text: "Modal" },
+  { name: "offcanvas", text: "Offcanvas" },
+  { name: "pagination", text: "Pagination" },
+  { name: "progress", text: "Progress" },
+  { name: "ratio", text: "Ratio" },
+  { name: "toast", text: "Toast" },
+];
 </script>
 
 <template>
