175fbcc6fee5178dd4f0251a810febacd5292043
Tomoki Morita <tmsongbooks215@gmail.com>
2022-03-24 01:25:19 +0900

    Update dependencies
    and minor fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ import purgecssConfig from "./purgecss.config";
 
 export default defineNuxtConfig({
   srcDir: "src/",
-  components: ["~/src/components"],
   typescript: {
     shim: false,
     strict: true,
