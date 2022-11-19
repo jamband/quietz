7bfce4e166f1608a6fc95c8d2158c55c4d2c7e49
Tomoki Morita <tmsongbooks215@gmail.com>
2022-11-19 18:04:23 +0900

    improve some commands in package.json

--- scripts/clean.mjs
+++ scripts/clean.mjs
@@ -1,6 +1,3 @@
 import { rm } from "node:fs/promises";
 
-await rm(".nuxt", { recursive: true, force: true });
-await rm(".output", { recursive: true, force: true });
-await rm("dist", { recursive: true, force: true });
 await rm("node_modules", { recursive: true, force: true });
