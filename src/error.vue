83fc0dfd8f5651dd64c8ab07fca11a3207fddfb3
Tomoki Morita <tmsongbooks215@gmail.com>
2022-11-19 21:51:14 +0900

    improve not found page
    this is a experimental commit

---
+++ src/error.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const error = useError();
+
+const isNotFound = Number(error.value?.statusCode || 500) === 404;
+const title = isNotFound ? "Not Found" : "An Error Occurred";
+const message = isNotFound ? "Page not found." : error.value?.message;
+</script>
+
+<template>
+  <NuxtLayout>
+    <ThePage :title="title" />
+    <h1>{{ title }}</h1>
+    <p>
+      <IconInfoCircleFill />
+      {{ message }}
+    </p>
+    <div class="my-5 text-center">
+      <NuxtLink to="/" class="p-4">Back to Home</NuxtLink>
+    </div>
+  </NuxtLayout>
+</template>
