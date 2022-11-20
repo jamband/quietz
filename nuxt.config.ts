937d3042732580bb725bec5ab68dd0381628be27
Tomoki Morita <tmsongbooks215@gmail.com>
2022-11-20 20:30:42 +0900

    remove some options in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,11 +5,6 @@ export default defineNuxtConfig({
   typescript: {
     shim: false,
   },
-  app: {
-    head: {
-      viewport: "width=device-width, initial-scale=1, viewport-fit=cover",
-    },
-  },
   css: ["~/assets/css/app.scss"],
   postcss: {
     plugins: {
