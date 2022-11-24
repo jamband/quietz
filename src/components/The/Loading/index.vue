a5912ababe899afae70e4f0439d602afec0b5486
Tomoki Morita <tmsongbooks215@gmail.com>
2022-11-24 19:39:23 +0900

    use TheLoading instead of NuxtLoadingIndicator
    and some style improvements

---
+++ src/components/The/Loading/index.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+const { afterEach, beforeEach } = useRouter();
+const state = ref<"initial" | "start" | "complete">("initial");
+
+beforeEach(() => {
+  state.value = "start";
+});
+
+afterEach(() => {
+  setTimeout(() => {
+    state.value = "complete";
+  }, 100);
+
+  setTimeout(() => {
+    state.value = "initial";
+  }, 500);
+});
+</script>
+
+<template>
+  <div
+    :class="[
+      $style.initial,
+      state === 'start' && $style.start,
+      state === 'complete' && $style.complete,
+    ]"
+    role="status"
+  />
+</template>
+
+<style module lang="scss" src="./styles.scss" />
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -12,7 +12,7 @@ const appConfig = useAppConfig();
         <Link rel="icon" href="/favicon.png" />
       </Head>
     </Html>
-    <NuxtLoadingIndicator color="#15803d" />
+    <TheLoading />
     <TheNotification />
     <TheHeader />
     <TheBreadcrumb />
